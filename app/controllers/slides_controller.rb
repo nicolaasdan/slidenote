80799d34e9c4468e03ad055deac9435d749cdf59
Nicolaas Danneels <nicolaasdanneels@gmail.com>
2016-03-14 16:42:02 +0100

    Added edit, update and destroy action in SlidesCtrl. You can now add, update and delete slides

--- app/controllers/slides_controller.rb
+++ app/controllers/slides_controller.rb
@@ -1,8 +1,10 @@
 class SlidesController < ApplicationController
   before_action :slide_params, only: [:create]
-  before_action :find_course, only: [:index, :create, :new]
+  before_action :find_course, only: [:index, :create, :new, :edit, :update, :destroy]
+  before_action :find_slide, only: [:edit, :updaten, :destroy]
 
   def index
+    @slides = Slide.all
   end
 
   def new
@@ -12,12 +14,28 @@ class SlidesController < ApplicationController
   def create
   	@slide = Slide.new(slide_params)
     @slide.course_id = @course.id
-    
+
   	if @slide.save
-  	  redirect to courses_path
+  	  redirect_to course_slides_path(@course)
   	else
   	  render 'new'
   	end
+  end
+
+  def edit
+  end
+
+  def update
+    if @slide.update(slide_params)
+      redirect_to course_slides_path(@course)
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @slide.destroy
+    redirect_to course_slides_path(@course)
   end
 
   private
@@ -28,5 +46,9 @@ class SlidesController < ApplicationController
 
     def find_course
       @course = Course.find(params[:course_id])
+    end
+
+    def find_slide
+      @slide = Slide.find(params[:id])
     end
 end
