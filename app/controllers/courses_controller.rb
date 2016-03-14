57a78b71e8cf672ba594582121232bb0f8557173
Nicolaas Danneels <nicolaasdanneels@gmail.com>
2016-03-14 15:01:23 +0100

    Added edit, update and destroy action in course controller

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -1,5 +1,6 @@
 class CoursesController < ApplicationController
-  before_action :course_params, only: [:create]
+  before_action :course_params, only: [:create, :update]
+  before_action :find_params, only: [:edit, :update, :destroy]
 
   def index
   	@courses = Course.all
@@ -18,9 +19,29 @@ class CoursesController < ApplicationController
   	end
   end
 
+  def edit
+  end
+
+  def update
+  	if @course.update(course_params)
+  	  redirect_to courses_path
+  	else
+  	  render 'edit'
+  	end
+  end
+
+  def destroy
+  	@course.destroy
+  	redirect_to courses_path
+  end
+
   private
 
     def course_params
       params.require(:course).permit(:name, :description)
+    end
+
+    def find_params
+      @course = Course.find(params[:id])
     end
 end
