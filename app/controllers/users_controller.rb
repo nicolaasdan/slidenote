3f7e8af06f2f1424f08cb856ee2104f72cea9e91
Nicolaas Danneels <nicolaasdanneels@gmail.com>
2016-03-19 11:17:55 +0100

    added first & lastname in profile with regex

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,10 @@
 class UsersController < ApplicationController
   def show
   	@user = current_user
+  	@name_regex = /^([^\.@]+)\.*([^@]*)/
+  	first, last = @user.email.match(@name_regex).captures
+  	@first_name = first	
+  	@last_name = last
   end
 end
+
